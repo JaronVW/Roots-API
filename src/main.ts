4c76c461b7f18803fac8a7772a66effc052d0101
Jaron van Well <jaronvwell@gmail.com>
2022-12-14 14:22:40 +0100

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Roots-API')
+    .setDescription('Roots API documentation')
+    .setVersion('1.0')
+    .addTag('Event')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();

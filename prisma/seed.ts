febf86a2e219add984b85cb644eae19f347a4675
Jaron van Well <jaronvwell@gmail.com>
2023-01-15 21:30:16 +0100

    translated seed data to Dutch

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -15,27 +15,27 @@ async function main() {
 
   // Tags
   const tag1 = {
-    subject: 'Finances',
+    subject: 'Financiën',
     organisationId: organisation1.id,
   };
 
   const tag2 = {
-    subject: 'Work culture',
+    subject: 'Werkcultuur',
     organisationId: organisation1.id,
   };
 
   const tag3 = {
-    subject: 'Adminstration',
+    subject: 'Administratie',
     organisationId: organisation1.id,
   };
 
   const tag4 = {
-    subject: 'Infrastructure',
+    subject: 'Infrastructuur',
     organisationId: organisation1.id,
   };
 
   const tag5 = {
-    subject: 'Relocation',
+    subject: 'Verhuizing',
     organisationId: organisation1.id,
   };
 
@@ -61,21 +61,21 @@ async function main() {
       events: {
         create: [
           {
-            title: 'Max 12 people per team in a BV',
-            description: 'We are going to limit the amount of people in a team in a BV to 12.',
+            title: 'Max twaalf mensen in een team per BV',
+            description: 'We gaan het aantal mensen in een team per BV terugbrengen naar twaalf.',
             content: '',
             dateOfEvent: new Date('2013-09-09T00:00:00.000Z'),
             tags: {
               connectOrCreate: [
                 {
                   where: {
-                    unique_tag_organisation: { subject: 'Changing team sizes', organisationId: organisation1.id },
+                    unique_tag_organisation: { subject: 'Wijziging teamgrootte', organisationId: organisation1.id },
                   },
-                  create: { subject: 'Changing team sizes', organisation: { connect: { id: organisation1.id } } },
+                  create: { subject: 'Wijziging teamgrootte', organisation: { connect: { id: organisation1.id } } },
                 },
                 {
-                  where: { unique_tag_organisation: { subject: 'Work environment', organisationId: organisation1.id } },
-                  create: { subject: 'Work environment', organisation: { connect: { id: organisation1.id } } },
+                  where: { unique_tag_organisation: { subject: 'Werkomgeving', organisationId: organisation1.id } },
+                  create: { subject: 'Werkomgeving', organisation: { connect: { id: organisation1.id } } },
                 },
               ],
             },
@@ -106,21 +106,21 @@ async function main() {
       events: {
         create: [
           {
-            title: 'More teams in a BV',
+            title: 'Meer teams in een BV',
             description:
-              'To improve the efficiency of the company, we are going to split the teams into smaller teams. This will improve the communication between the teams and the efficiency of the company.',
+              'Om de teams te laten groeien, gaan we meer teams in een BV toestaan. Dit is een proef, we kijken of dit goed werkt.',
             content:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas orci a urna vestibulum, vel blandit felis aliquet. Pellentesque posuere turpis ut ultrices fringilla. Donec interdum nisl quis sem mattis, ac finibus elit ultrices. Duis efficitur faucibus luctus. Nam porta scelerisque urna, ut eleifend leo facilisis vel. Maecenas non quam pellentesque, maximus libero quis, gravida elit. Pellentesque ac ornare nisl, sit amet consequat eros. Mauris lorem justo, ullamcorper nec pretium id, eleifend vel leo. \nVivamus nec sagittis lorem. Mauris vitae suscipit lectus. Duis tortor ipsum, fermentum id ante non, ullamcorper hendrerit lectus. Vivamus semper velit sit amet libero ultricies aliquam. Integer a dictum ex, et gravida tortor. Integer cursus neque sed consectetur tincidunt. In porttitor aliquet lorem vitae rhoncus. Vivamus non libero cursus, accumsan dolor a, pharetra risus. Duis dignissim metus ut lorem consequat, ac sodales libero cursus. Fusce pharetra maximus fringilla. Sed tempus dignissim odio ut molestie. In sit amet bibendum urna, at euismod risus. Duis hendrerit velit ac tellus placerat, et sodales ex sollicitudin.',
+              '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas orci a urna vestibulum, vel blandit felis aliquet. Pellentesque posuere turpis ut ultrices fringilla. Donec interdum nisl quis sem mattis, ac finibus elit ultrices. Duis efficitur faucibus luctus. Nam porta scelerisque urna, ut eleifend leo facilisis vel. Maecenas non quam pellentesque, maximus libero quis, gravida elit. Pellentesque ac ornare nisl, sit amet consequat eros. Mauris lorem justo, ullamcorper nec pretium id, eleifend vel leo. \nVivamus nec sagittis lorem. Mauris vitae suscipit lectus. Duis tortor ipsum, fermentum id ante non, ullamcorper hendrerit lectus. Vivamus semper velit sit amet libero ultricies aliquam. Integer a dictum ex, et gravida tortor. Integer cursus neque sed consectetur tincidunt. In porttitor aliquet lorem vitae rhoncus. Vivamus non libero cursus, accumsan dolor a, pharetra risus. Duis dignissim metus ut lorem consequat, ac sodales libero cursus. Fusce pharetra maximus fringilla. Sed tempus dignissim odio ut molestie. In sit amet bibendum urna, at euismod risus. Duis hendrerit velit ac tellus placerat, et sodales ex sollicitudin.</p>',
             dateOfEvent: new Date('2021-01-01T00:00:00.000Z'),
             tags: {
               connectOrCreate: [
                 {
-                  where: { unique_tag_organisation: { subject: 'Smaller teams', organisationId: organisation1.id } },
-                  create: { subject: 'Smaller teams', organisation: { connect: { id: organisation1.id } } },
+                  where: { unique_tag_organisation: { subject: 'Kleinere teams', organisationId: organisation1.id } },
+                  create: { subject: 'Kleinere teams', organisation: { connect: { id: organisation1.id } } },
                 },
                 {
-                  where: { unique_tag_organisation: { subject: 'Splitting teams', organisationId: organisation1.id } },
-                  create: { subject: 'Splitting teams', organisation: { connect: { id: organisation1.id } } },
+                  where: { unique_tag_organisation: { subject: 'Teams splitten', organisationId: organisation1.id } },
+                  create: { subject: 'Teams splitten', organisation: { connect: { id: organisation1.id } } },
                 },
               ],
             },
@@ -131,20 +131,20 @@ async function main() {
             },
           },
           {
-            title: 'New office',
-            description: 'We had to move locations, the main office is now in Breda.',
+            title: 'Nieuw kantoorkantoor',
+            description: 'We zijn verhuisd naar een nieuwe locatie, het nieuwe kantoor is in Breda.',
             content:
-              'Nulla nec metus sit amet tortor fermentum faucibus. Pellentesque rhoncus eros a elit venenatis, a facilisis augue venenatis. Phasellus ante erat, lacinia id odio lacinia, condimentum finibus magna. Pellentesque viverra, leo in accumsan tincidunt, lacus odio sagittis nibh, vulputate posuere nisl justo ut eros. Ut mattis arcu eget semper tempor. Cras feugiat quis sem at efficitur. Nulla facilisi.',
+              '<p>Nulla nec metus sit amet tortor fermentum faucibus. Pellentesque rhoncus eros a elit venenatis, a facilisis augue venenatis. Phasellus ante erat, lacinia id odio lacinia, condimentum finibus magna. Pellentesque viverra, leo in accumsan tincidunt, lacus odio sagittis nibh, vulputate posuere nisl justo ut eros. Ut mattis arcu eget semper tempor. Cras feugiat quis sem at efficitur. Nulla facilisi.</p>',
             dateOfEvent: new Date('2019-05-01T00:00:00.000Z'),
             tags: {
               connectOrCreate: [
                 {
-                  where: { unique_tag_organisation: { subject: 'Moving', organisationId: organisation1.id } },
-                  create: { subject: 'Moving', organisation: { connect: { id: organisation1.id } } },
+                  where: { unique_tag_organisation: { subject: 'Verhuizing', organisationId: organisation1.id } },
+                  create: { subject: 'Verhuizing', organisation: { connect: { id: organisation1.id } } },
                 },
                 {
-                  where: { unique_tag_organisation: { subject: 'New office', organisationId: organisation1.id } },
-                  create: { subject: 'New office', organisation: { connect: { id: organisation1.id } } },
+                  where: { unique_tag_organisation: { subject: 'Nieuw kantoor', organisationId: organisation1.id } },
+                  create: { subject: 'Nieuw kantoor', organisation: { connect: { id: organisation1.id } } },
                 },
               ],
             },
@@ -175,24 +175,24 @@ async function main() {
       events: {
         create: [
           {
-            title: '20 people per team is the new norm',
+            title: 'Twitig mensen per team is de nieuwe norm',
             description:
-              'We are going to limit the amount of people in a team to 20 as bigger teams tend to cause chaos and some people fall to the background.',
+              'We gaan het aantal mensen per team verminderen naar twintig mensen. Dit zal de communicatie tussen de teams en de efficiëntie van de organisatie verbeteren.',
             content:
-              'Proin rhoncus dapibus quam, at tempus ante semper sit amet. Suspendisse pretium sed nunc sed tempor. Phasellus sagittis posuere tempor. In commodo eros sed massa blandit, eu volutpat metus ornare. Etiam libero nisi, facilisis vitae ante sodales, faucibus fermentum neque. Ut et eleifend turpis. Donec vehicula mi purus, sit amet efficitur nibh molestie quis. Proin quis lectus quis odio sagittis vehicula. Cras a purus eget dui efficitur suscipit. Sed a velit leo. \nUt ut urna risus. In eleifend lectus sit amet facilisis consequat. Phasellus malesuada faucibus turpis, a ornare quam consectetur ut. Donec egestas aliquet dui, ac eleifend tellus imperdiet at. Quisque rhoncus tincidunt ipsum. Maecenas nec imperdiet libero, ut iaculis mauris. Sed ac gravida magna, semper elementum neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce non imperdiet magna, vitae lobortis sapien. Duis sodales eu justo quis feugiat. Vestibulum massa magna, malesuada ut nisi eget, feugiat maximus urna. Sed maximus tellus sit amet est porttitor, vitae efficitur arcu iaculis. Curabitur porttitor mi interdum mauris scelerisque rutrum. Curabitur nec mauris tempus, bibendum est in, pulvinar mauris. Nulla commodo nibh diam, a hendrerit turpis faucibus ut.',
+              '</p>Proin rhoncus dapibus quam, at tempus ante semper sit amet. Suspendisse pretium sed nunc sed tempor. Phasellus sagittis posuere tempor. In commodo eros sed massa blandit, eu volutpat metus ornare. Etiam libero nisi, facilisis vitae ante sodales, faucibus fermentum neque. Ut et eleifend turpis. Donec vehicula mi purus, sit amet efficitur nibh molestie quis. Proin quis lectus quis odio sagittis vehicula. Cras a purus eget dui efficitur suscipit. Sed a velit leo. \nUt ut urna risus. In eleifend lectus sit amet facilisis consequat. Phasellus malesuada faucibus turpis, a ornare quam consectetur ut. Donec egestas aliquet dui, ac eleifend tellus imperdiet at. Quisque rhoncus tincidunt ipsum. Maecenas nec imperdiet libero, ut iaculis mauris. Sed ac gravida magna, semper elementum neque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce non imperdiet magna, vitae lobortis sapien. Duis sodales eu justo quis feugiat. Vestibulum massa magna, malesuada ut nisi eget, feugiat maximus urna. Sed maximus tellus sit amet est porttitor, vitae efficitur arcu iaculis. Curabitur porttitor mi interdum mauris scelerisque rutrum. Curabitur nec mauris tempus, bibendum est in, pulvinar mauris. Nulla commodo nibh diam, a hendrerit turpis faucibus ut.</p>',
             dateOfEvent: new Date('2008-12-11T00:00:00.000Z'),
             tags: {
               connectOrCreate: [
                 {
                   where: {
-                    unique_tag_organisation: { subject: 'Changing team sizes', organisationId: organisation1.id },
+                    unique_tag_organisation: { subject: 'Wijziging teamgrootte', organisationId: organisation1.id },
                   },
-                  create: { subject: 'Changing team sizes', organisation: { connect: { id: organisation1.id } } },
+                  create: { subject: 'Wijziging teamgrootte', organisation: { connect: { id: organisation1.id } } },
                 },
                 {
-                  where: { unique_tag_organisation: { subject: 'Work environment', organisationId: organisation1.id } },
+                  where: { unique_tag_organisation: { subject: 'Werkomgeving', organisationId: organisation1.id } },
                   create: {
-                    subject: 'Work environment',
+                    subject: 'Werkomgeving',
                     organisation: { connect: { id: organisation1.id } },
                   },
                 },
